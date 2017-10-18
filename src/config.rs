a823e358893875a7fe33d407ca6c4d3787aa7a65
timglabisch <tim@ainfach.de>
2017-10-18 22:52:39 +0200

    allow to read configuration directly from serverdensitys configuration file

---
+++ src/config.rs
@@ -0,0 +1,53 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+#[derive(Clone)]
+pub struct Config {
+    pub token: String,
+    pub account_url: String,
+    pub agent_key: String,
+    pub serverdensity_endpoint: String,
+    pub debug: bool,
+    pub bind: String
+}
+
+impl Config {
+
+    fn line_value(&self, line : &str) -> String {
+        let value = line.trim().split(":").map(|x|x.trim().to_string()).collect::<Vec<String>>();
+        
+        if value.len() != 2 {
+            return "".to_string();
+        }
+
+        return value[1].clone();
+    }
+
+    pub fn apply_config_file(&mut self, filename: &str) -> Result<(), String> {
+        let file = File::open(filename).or_else(|_| Err("could not open config file".to_string()))?;
+        let mut buf_reader = BufReader::new(file);
+        
+        let mut content = String::new();
+        
+        buf_reader.read_to_string(&mut content).or_else(|_| Err("could not read config file".to_string()))?;
+
+        for line in content.split("\n") {
+            if line.trim().starts_with("#") || line.trim().starts_with("[")  {
+                continue;
+            }
+
+            if line.trim().starts_with("agent_key") {
+                self.agent_key = self.line_value(&line);
+                continue;
+            }
+
+            if line.trim().starts_with("sd_account") {
+                self.account_url = self.line_value(&line);
+                continue;
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ extern crate reqwest;
 #[macro_use] extern crate hyper;
 extern crate regex;
 
+mod config;
+
 use clap::{Arg, App};
 use std::net::UdpSocket;
 use byteorder::{BigEndian};
@@ -17,6 +19,7 @@ use std::time::{SystemTime, Duration};
 use std::io::Read;
 use regex::Regex;
 use std::sync::mpsc::TryRecvError;
+use config::Config;
 
 header! { (XForwardedHost, "X-Forwarded-Host") => [String] }
 
@@ -25,16 +28,6 @@ header! { (XForwardedHost, "X-Forwarded-Host") => [String] }
 struct Metric {
     pub name: String,
     pub count: i32
-}
-
-#[derive(Clone)]
-struct Config {
-    pub token: String,
-    pub account_url: String,
-    pub agent_key: String,
-    pub serverdensity_endpoint: String,
-    pub debug: bool,
-    pub bind: String
 }
 
 fn main() {
@@ -46,17 +39,17 @@ fn main() {
         .arg(Arg::with_name("token")
             .help("Server Density API Token")
             .long("token")
-            .required(true)
+            .required(false)
             .takes_value(true))
         .arg(Arg::with_name("account-url")
             .help("Set this to your Server Density account url, e.g. example.serverdensity.io")
             .long("account-url")
-            .required(true)
+            .required(false)
             .takes_value(true))
         .arg(Arg::with_name("agent-key")
             .help("This is the agent key used to identify the device when payloads are processed. You can find this in the top left corner when you view a device page in your UI")
             .long("agent-key")
-            .required(true)
+            .required(false)
             .takes_value(true))
         .arg(Arg::with_name("serverdensity-endpoint")
             .default_value("https://api.serverdensity.io")
@@ -75,23 +68,46 @@ fn main() {
             .help("verbose mode, just for debugging")
             .long("debug")
             .takes_value(false))
+        .arg(Arg::with_name("config")
+            .short("c")
+            .help("path to the serverdensity config file, may /etc/sd-agent/config.cfg?")
+            .long("config")
+            .takes_value(true))
         .get_matches();
 
-    let config = Config {
+    let mut config = Config {
         token: matches.value_of("token").unwrap().to_string(),
-        account_url: matches.value_of("account-url").unwrap().to_string(),
-        agent_key: matches.value_of("agent-key").unwrap().to_string(),
+        account_url: matches.value_of("account-url").unwrap_or("").to_string(),
+        agent_key: matches.value_of("agent-key").unwrap_or("").to_string(),
         serverdensity_endpoint: matches.value_of("serverdensity-endpoint").unwrap().to_string(),
         debug: matches.is_present("debug"),
         bind: matches.value_of("bind").unwrap().to_string()
     };
 
     println!("UDP Sender for Serverdendity");
+    
+    if matches.is_present("config") {
+        let config_file = matches.value_of("config").unwrap().to_string();
+        match config.apply_config_file(&config_file) {
+            Ok(_) => println!("successfully read config_file: {}", &config_file),
+            Err(_) => {
+                println!("could not read config_file: {}", &config_file);
+                return;
+            }
+        };
+    }
+
     println!("account-url: {}", &config.account_url);
     println!("agent-key: {}", &config.agent_key);
     println!("endpoint: {}", &config.serverdensity_endpoint);
     println!("debug: {:?}", &config.debug);
     println!("bind: {}", &config.bind);
+
+    if config.agent_key.trim() == "" || config.account_url.trim() == "" {
+        println!("agent-key or account-url not given.");
+        return;
+    }
+
     println!("\nServer is starting...");
 
     let (sender, receiver) = channel::<Metric>();
