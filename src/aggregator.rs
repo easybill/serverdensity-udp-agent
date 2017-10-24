f26197b6983ff0e3f1f9c50ed6ac4d0fa652de0b
timglabisch <tim@ainfach.de>
2017-10-24 19:14:07 +0200

    allow more complex metric names

--- src/aggregator.rs
+++ src/aggregator.rs
@@ -114,24 +114,66 @@ impl<'a> Aggregator<'a> {
         }
     }
 
+    pub fn create_plugin_map(map : &HashMap<String, i32>) -> String
+    {
+        let mut outermap : HashMap<String, HashMap<String, i32>> = HashMap::new();
+
+        for (k, v) in map {
+
+            let len = k.len();
+
+            match k.find(".") {
+                Some(index) if index > 0 && index + 1 != len => {
+                    outermap
+                    .entry(k[..index].to_string())
+                    .or_insert(HashMap::new())
+                    .insert(k[index + 1..].to_string(), v.clone());
+                },
+                _ if k.trim() != "" => {
+                    outermap
+                    .entry("custom".to_string())
+                    .or_insert(HashMap::new())
+                    .insert(k.to_string(), v.clone());
+                }
+                _ => {}
+            };
+        }
+
+        outermap.iter().map(|(k, v)|{
+            let mut buf = String::new();
+            buf.push_str("\"");
+            buf.push_str(k);
+            buf.push_str("\":{");
+
+            buf.push_str(
+                &v.iter().map(|(k, v)| {
+                    format!("\"{}\":\"{}\"", k, v)
+                })
+                .collect::<Vec<String>>()
+                .join(",")
+                .to_string()
+            );
+
+            buf.push_str("}");
+            buf
+        })
+        .collect::<Vec<String>>()
+        .join(",")
+        .to_string()
+    }
+
     pub fn push_to_serverdensity(&self, metricmap : &mut HashMap<String, i32>)
     {
         if metricmap.len() == 0 {
             return;
         }
 
-        let x = metricmap.iter().map(|(k, v)| {
-            format!("\"{}\":\"{}\"", k, v)
-        })
-        .collect::<Vec<String>>()
-        .join(",")
-        .to_string();
         
         let mut payload = "{\"agentKey\":\"".to_string();
         payload.push_str(&self.config.agent_key);
-        payload.push_str("\",\"plugins\":{\"website\":{");
-        payload.push_str(&x);
-        payload.push_str("}}}");
+        payload.push_str("\",\"plugins\":{");
+        payload.push_str(&Self::create_plugin_map(metricmap));
+        payload.push_str("}}");
 
         *metricmap = HashMap::new();
 
@@ -181,3 +223,51 @@ impl<'a> Aggregator<'a> {
     }
 
 }
+
+#[cfg(test)]
+    mod tests {
+
+        use ::std::collections::HashMap;
+        use ::aggregator::Aggregator;
+
+        #[test]
+        fn it_works() {
+
+            let mut m = HashMap::new();
+            m.insert("foo".to_string(), 2);
+            m.insert("foo.".to_string(), 3);
+            m.insert(".foo.bar.barr".to_string(), 4);
+
+            m.insert("foo.bar".to_string(), 5);
+            m.insert("foo.bar.baz".to_string(), 6);
+
+            let out = Aggregator::create_plugin_map(&m);
+
+            println!("{}\n", &format!("{}", out));
+
+
+            let mut m = HashMap::new();
+            m.insert("foo".to_string(), 2);
+            let out = Aggregator::create_plugin_map(&m);
+            println!("{}\n", &format!("{}", out));
+
+            let mut m = HashMap::new();
+            m.insert("foo.bar".to_string(), 2);
+            let out = Aggregator::create_plugin_map(&m);
+            println!("{}\n", &format!("{}", out));
+
+            let mut m = HashMap::new();
+            let out = Aggregator::create_plugin_map(&m);
+            println!("{}\n", &format!("{}", out));
+
+            /*
+            the assert doesnt work because the map is not ordered.
+
+            assert_eq!(
+                r#""foo":{"bar":"5","bar.baz":"6"},"custom":{".foo.bar.barr":"4","foo":"2","foo.":"3"}"#,
+                &format!("{}", out)
+            );
+            */
+
+        }
+    }
