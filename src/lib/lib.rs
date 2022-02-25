9208c6a1f03c178ebe03cd1aece7047cb63a9bab
timglabisch <glabisch@easybill.de>
2022-02-25 10:23:42 +0100

    lib now supports createing packages

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,13 +1,15 @@
+use bytes::{BufMut, BytesMut};
+
 #[derive(Debug, PartialEq)]
 pub enum MetricType {
     SUM,
     AVERAGE,
     PEAK,
-    MIN
+    MIN,
 }
 
 impl MetricType {
-    pub fn from_u16(v : u16) -> Option<MetricType>
+    pub fn from_u16(v: u16) -> Option<MetricType>
     {
         match v {
             42 => Some(MetricType::SUM),
@@ -17,20 +19,47 @@ impl MetricType {
             _ => None
         }
     }
+
+    pub fn to_u16(&self) -> u16 {
+        match self {
+            Self::SUM => 42,
+            Self::AVERAGE => 43,
+            Self::PEAK => 44,
+            Self::MIN => 45
+        }
+    }
 }
 
-pub fn create_package() {
+pub fn create_package<S>(metric_type: MetricType, name: S, count: i32) -> Result<Vec<u8>, &'static str>
+    where S: AsRef<str> {
+    let mut buf = BytesMut::new();
+    buf.put_u16(metric_type.to_u16());
+    buf.put_i32(count);
+    buf.put_slice(name.as_ref().as_bytes());
 
-    /*
-    let metric_type = match MetricType::from_u16(BigEndian::read_u16(&buf[0..2])) {
-        Some(m) => m,
-        None => {
-            return Err("unsupported metric type".to_string());
-        }
-    };
+    if buf.len() > 300 {
+        return Err("could not create a package that is larger than 300 bytes!");
+    }
 
-    let count = BigEndian::read_i32(&buf[2..6]);
-    let name = String::from_utf8_lossy(&buf[6..amt]).to_string().replace("\"", "");
+    Ok(buf.to_vec())
+}
 
-     */
+pub fn create_package_sum<S>(name: S, count: i32) -> Result<Vec<u8>, &'static str>
+    where S: AsRef<str> {
+    create_package(MetricType::SUM, name, count)
+}
+
+pub fn create_package_min<S>(name: S, count: i32) -> Result<Vec<u8>, &'static str>
+    where S: AsRef<str> {
+    create_package(MetricType::MIN, name, count)
+}
+
+pub fn create_package_peak<S>(name: S, count: i32) -> Result<Vec<u8>, &'static str>
+    where S: AsRef<str> {
+    create_package(MetricType::PEAK, name, count)
+}
+
+pub fn create_package_average<S>(name: S, count: i32) -> Result<Vec<u8>, &'static str>
+    where S: AsRef<str> {
+    create_package(MetricType::AVERAGE, name, count)
 }
