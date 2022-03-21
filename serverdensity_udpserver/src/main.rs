bcbbfa809ad7f96d7d81b4f489885bc90e2e91c7
timglabisch <glabisch@easybill.de>
2022-03-21 16:03:05 +0100

    use rusttls

--- serverdensity_udpserver/src/main.rs
+++ serverdensity_udpserver/src/main.rs
@@ -4,7 +4,6 @@ extern crate url;
 extern crate md5;
 extern crate reqwest;
 extern crate regex;
-extern crate openssl_probe;
 
 mod config;
 mod aggregator;
@@ -27,8 +26,6 @@ pub struct Metric {
 }
 
 fn main() {
-
-    ::openssl_probe::init_ssl_cert_env_vars();
 
     let matches = App::new("Server Density UDP Monitor")
         .version("1.0")
