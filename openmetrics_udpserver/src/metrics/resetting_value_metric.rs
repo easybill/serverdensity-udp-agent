fbc53b5d042598089ef3f34b8520759b5c8845b5
Christoph Wersal <christoph.wersal@tngtech.com>
2023-12-13 17:58:40 +0100

    Use Prometheus type "gauge" for non-counter metrics
    * still only returns the latest value and not acutally the peak/min/avg
    * if floats are sent via UDP, the value will be rounded to an integer

--- openmetrics_udpserver/src/metrics/resetting_value_metric.rs
+++ openmetrics_udpserver/src/metrics/resetting_value_metric.rs
@@ -1,5 +1,6 @@
 use std::fmt::Error;
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::Ordering;
+use atomic_float::AtomicF64;
 use std::sync::Arc;
 
 use crate::metrics::ModifyMetric;
@@ -8,13 +9,13 @@ use prometheus_client::metrics::{MetricType, TypedMetric};
 
 #[derive(Debug, Default, Clone)]
 pub struct ResettingSingleValMetric {
-    val: Arc<AtomicU64>,
+    val: Arc<AtomicF64>,
 }
 
 impl ModifyMetric for ResettingSingleValMetric {
     fn observe(&self, value: i32) {
-        if let Ok(val_as_u64) = u64::try_from(value) {
-            self.val.store(val_as_u64, Ordering::Relaxed);
+        if let Ok(val_as_f64) = f64::try_from(value) {
+            self.val.store(val_as_f64, Ordering::Relaxed);
         }
     }
 }
@@ -22,7 +23,7 @@ impl ModifyMetric for ResettingSingleValMetric {
 impl EncodeMetric for ResettingSingleValMetric {
     fn encode(&self, mut encoder: MetricEncoder) -> Result<(), Error> {
         let current_value = self.val.load(Ordering::Relaxed);
-        encoder.encode_counter::<(), u64, u64>(&current_value, None)
+        encoder.encode_gauge::<f64>(&current_value)
     }
 
     fn metric_type(&self) -> MetricType {
@@ -31,5 +32,5 @@ impl EncodeMetric for ResettingSingleValMetric {
 }
 
 impl TypedMetric for ResettingSingleValMetric {
-    const TYPE: MetricType = MetricType::Counter;
+    const TYPE: MetricType = MetricType::Gauge;
 }
