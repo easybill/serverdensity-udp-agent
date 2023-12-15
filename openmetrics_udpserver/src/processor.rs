7df529653b31070f9e83564c04e96e262c520d51
timglabisch <glabisch@easybill.de>
2023-12-15 13:49:07 +0100

    ...

--- openmetrics_udpserver/src/processor.rs
+++ openmetrics_udpserver/src/processor.rs
@@ -25,6 +25,22 @@ pub struct Processor {
     metric_registry: Arc<RwLock<Registry>>,
 }
 
+pub struct ProcessorMetric {
+    pub name: String,
+    pub count: i32,
+    pub metric_type: MetricType,
+}
+
+impl ProcessorMetric {
+    pub fn from_inbound(name: String, inbound_metric: InboundMetric) -> Self {
+        Self {
+            name,
+            count: inbound_metric.count,
+            metric_type: inbound_metric.metric_type,
+        }
+    }
+}
+
 impl Processor {
     pub fn new(config: Config, metric_registry: Arc<RwLock<Registry>>) -> Self {
         Processor {
@@ -36,24 +52,32 @@ impl Processor {
     }
 
     pub async fn run(&mut self, mut receiver: Receiver<InboundMetric>) {
+        let regex_allowed_chars = Regex::new(r"^[^a-zA-Z_:]|[^a-zA-Z0-9_:]")
+            .expect("Unable to compile metrics naming regex, should not happen");
         loop {
             match receiver.recv().await {
                 Ok(inbound_metric) => {
-                    if inbound_metric.name.trim().is_empty() {
+                    let metric_name = regex_allowed_chars
+                        .replace_all(&inbound_metric.name.replace('.', "_"), "")
+                        .trim()
+                        .to_string();
+                    if metric_name.is_empty() {
                         eprintln!("got empty metric name");
                         continue;
                     }
 
+                    let processor_metric = ProcessorMetric::from_inbound(metric_name, inbound_metric);
+
                     if self.config.debug {
                         println!(
                             "got metric [type={:?}, name={}, count={}]",
-                            &inbound_metric.metric_type, &inbound_metric.name, &inbound_metric.count
+                            &processor_metric.metric_type, &processor_metric.name, &processor_metric.count
                         );
                     }
 
-                    match inbound_metric.metric_type {
-                        MetricType::Min | MetricType::Average | MetricType::Peak => self.handle_gauge(&inbound_metric),
-                        MetricType::Sum => self.handle_counter(&inbound_metric),
+                    match processor_metric.metric_type {
+                        MetricType::Min | MetricType::Average | MetricType::Peak => self.handle_gauge(&processor_metric).await,
+                        MetricType::Sum => self.handle_counter(&processor_metric).await,
                     }
                 }
                 Err(e) => {
@@ -64,28 +88,38 @@ impl Processor {
         }
     }
 
-    fn handle_counter(&mut self, inbound_metric: &InboundMetric) {
-        match self.counters.entry(inbound_metric.name.clone()) {
+    async fn handle_counter(&mut self, metric: &ProcessorMetric) {
+        match self.counters.entry(metric.name.clone()) {
             Entry::Occupied(mut v) => {
-                v.get_mut().inc_by(inbound_metric.count as u64);
+                v.get_mut().inc_by(metric.count as u64);
             },
             Entry::Vacant(vacant) => {
                 let counter = Counter::<u64, AtomicU64>::default();
-                counter.inc_by(inbound_metric.count as u64);
-                vacant.insert(counter);
+                counter.inc_by(metric.count as u64);
+                vacant.insert(counter.clone());
+
+                {
+                    let mut registry = self.metric_registry.write().await;
+                    registry.register(metric.name.clone(), metric.name.clone(), counter)
+                }
             }
         }
     }
 
-    fn handle_gauge(&mut self, inbound_metric: &InboundMetric) {
-        match self.gauges.entry(inbound_metric.name.clone()) {
+    async fn handle_gauge(&mut self, metric: &ProcessorMetric) {
+        match self.gauges.entry(metric.name.clone()) {
             Entry::Occupied(mut v) => {
-                v.get_mut().set(inbound_metric.count as i64);
+                v.get_mut().set(metric.count as i64);
             },
             Entry::Vacant(vacant) => {
                 let mut gauge = Gauge::<i64, AtomicI64>::default();
-                gauge.set(inbound_metric.count as i64);
-                vacant.insert(gauge);
+                gauge.set(metric.count as i64);
+                vacant.insert(gauge.clone());
+
+                {
+                    let mut registry = self.metric_registry.write().await;
+                    registry.register(metric.name.clone(), metric.name.clone(), gauge)
+                }
             }
         }
     }
